347f3baf2518bb91d65a8acd6f453e0c7b3b6076
Hathai <50056323+Hathai272@users.noreply.github.com>
2021-07-31 10:59:49 +0700

    Add files via upload

---
+++ findindex.java
@@ -0,0 +1,13 @@
+public class findindex {
+
+    public static void main (String[] args) {
+        
+        String s = "I am engineer";
+ 
+        for (int i = 1; i < s.length(); i++) {
+            
+        }
+        System.out.print(s.indexOf("g"));
+    }
+
+}
---
+++ triangle.java
@@ -0,0 +1,13 @@
+public class triangle {
+
+    public static void main (String[] args) {
+        int num = 0;
+        for (int i = 0; i <= 4; i++) {
+            for ( int j = 0 ; j <= i ; j++ ) {
+              System.out.print(i);
+              num++;
+           }
+           System.out.println();
+        } 
+    }
+}

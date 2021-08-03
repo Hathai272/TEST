d5929b05a923edb9b10c379a28f1795c8280aac0
Hathai <50056323+Hathai272@users.noreply.github.com>
2021-08-03 11:40:40 +0700

    Add files via upload
    fix for loop

--- findindex.java
+++ findindex.java
@@ -3,11 +3,18 @@ public class findindex {
     public static void main (String[] args) {
         
         String s = "I am engineer";
- 
-        for (int i = 1; i < s.length(); i++) {
-            
-        }
-        System.out.print(s.indexOf("g"));
+        char character = 'g';
+        int indexFirst = 1;
+        
+        for(int index = 1; index < s.length(); index++)
+          {
+            if(s.charAt(index) == character)
+            {
+              indexFirst = index+1;
+              break;
+            }
+          }
+        System.out.println(indexFirst);
     }
 
 }
